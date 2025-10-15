c08790b6285e29b9fd775d748139f8612bcc7f5b
diego cruz <diego.cruz02.bp@gmail.com>
2025-10-15 11:22:43 -0600

    Agrego gemfile, Procfile, gemfile.lock (se genera al ejecutar bundler install)

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec ruby app.rb -o 0.0.0.0 -p {$PORT}
+
---
+++ public/script.js
@@ -0,0 +1,28 @@
+async function cargarGrafica() {
+  const respuesta = await fetch('/data');
+  const datos = await respuesta.json();
+
+  const ctx = document.getElementById('grafica').getContext('2d');
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: datos.labels,
+      datasets: [{
+        label: 'Ventas mensuales 2025',
+        data: datos.values,
+        
+      }]
+    },
+    options:{
+        responsive: true,
+        color:'red',
+        plugins: {
+      legend: {
+        position: 'top',
+      }
+    }
+}
+  });
+}
+
+cargarGrafica();
