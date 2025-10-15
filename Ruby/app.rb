50d21a8695c2dcf010d8b1924bbe4d58f93ff36c
Diego Cruz <diego.cruz02.bp@gmail.com>
2025-10-15 08:41:37 -0600

    Add files via upload

---
+++ Ruby/app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'json'
+
+set :public_folder, 'public' # para archivos JS/CSS
+
+# Ruta principal
+get '/' do
+  erb :index
+end
+
+# Ruta que devuelve datos (simulados)
+get '/data' do
+  content_type :json
+  {
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+    values: [10, 20, 40, 50, 20]
+  }.to_json
+end
